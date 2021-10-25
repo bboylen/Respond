d7c7e2bc1a3424c6a626f4191d28e3be0633182c
Brian <bbbrianboylen@gmail.com>
2021-10-24 17:44:11 -0700

    added createElement function

---
+++ app.js
@@ -0,0 +1,38 @@
+// const element = {
+//   type: "h1",
+//   props: {
+//     title: "foo",
+//     children: "Hello",
+//   },
+// }
+
+// const container = document.getElementById("root");
+
+// const node = document.createElement(element.type);
+// node["title"] = elements.props.title;
+// const text = document.createTextNode("");
+// text["nodeValue"] = element.props.children;
+// node.appendChild(text);
+// container.appendChild(node);
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
